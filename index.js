a71f9068d06e443a54d2ab398a59dd4f42259af7
horlarhyinka <danijufarouq2003@gmail.com>
2022-06-09 20:11:41 +0100

    resolved conflict

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const express = require("express");
 const app = express();
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-const axios = require('axios');
-const cors = require('cors');
-const user = require('./models/usermodels.js');
-const bodyparser = require('body-parser');
-const router = require('./controller/routes-controller');
-=======
->>>>>>> 00bbc76364abee2b83a365d334f95413f47fcf3c
 const dotenv = require("dotenv");
 const router = require('./controller/routes-controller.js');
 const cookieparser = require('cookie-parser');
@@ -18,23 +9,6 @@ const port = Number(process.env.PORT) || 5000;
 //config .env file
 dotenv.config();
 
-<<<<<<< HEAD
-mongoose.promise = global.promise
-//declaring portt variable
-const port = Number(process.env.PORT) || 5000;
-
-//connecting to db:
-
-//setting up middlewares
-// app.use( morgan('dev') );
-// app.use( cors );
-app.use(express.static("static"));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-//seting up routes middleware
-app.use(router);
-//setting up error middleware
-=======
 //setting up middlewares;
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -54,7 +28,6 @@ app.use((err,req,res,next)=>{
 })
 
 
->>>>>>> 00bbc76364abee2b83a365d334f95413f47fcf3c
 
 //listening to port
 app.listen( port ,()=>{
